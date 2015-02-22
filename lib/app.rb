0f3eb87c54a21e4130a2fb415bbd48962257e82d
Diego Guerra <diego.guerra.suarez@gmail.com>
2015-02-22 18:04:48 +0100

    move config to its own file

--- lib/app.rb
+++ lib/app.rb
@@ -1,21 +1,5 @@
 require 'sinatra'
-require 'mongoid'
-
-if development?
-  require 'better_errors'
-  require "sinatra/reloader" 
-end
-
-configure do
-  set :bind, "0.0.0.0"
-  Mongoid.load!("config/mongoid.yml")
-  Dir["models/**/*.rb"].each {|model| require_relative model}
-end
-
-configure :development do
-  use BetterErrors::Middleware  
-  BetterErrors.application_root = File.expand_path('..', __FILE__)
-end
+require_relative 'config'
 
 get "/queue" do
   @queue = build_sample_queue
