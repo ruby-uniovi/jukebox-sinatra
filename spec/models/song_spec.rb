680f701e95b651d315921ef71752111b1948559c
Diego Guerra <diego.guerra.suarez@gmail.com>
2015-02-22 18:43:12 +0100

    tests for song creation

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -2,10 +2,31 @@ require 'spec_helper'
 
 describe Song do
 
-  it "resets the test database each time it runs" do
-    Song.create(:artist => "Velvet Underground", :title => "Venus in Furs")
-    Song.create(:artist => "David Bowie", :title => "Rebel, Rebel")
-    expect(Song.count).to eql(2)
+  context "test running" do
+    it "resets the test database each time it runs" do
+      Song.create(:artist => "Velvet Underground", :title => "Venus in Furs")
+      Song.create(:artist => "David Bowie", :title => "Rebel, Rebel")
+      expect(Song.count).to eql(2)
+    end
   end
+
+  context "creation" do
+
+    let(:song) {Song.create(:artist => "Art Blakey & The Jazz Messengers", :title => "A Night in Tunisia", :url => "http://example.com/nothing_here")}
+
+    it "has a title" do
+      expect(song.title).to eql("A Night in Tunisia")
+    end
+
+    it "has an artist" do
+      expect(song.artist).to eql("Art Blakey & The Jazz Messengers")
+    end
+
+    it "has a URL" do
+      expect(song.url).to eql("http://example.com/nothing_here")
+    end
+  end
+
+
 
 end
