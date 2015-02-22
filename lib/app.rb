7f8f61cab0fd3749fe66a01873c8751a2db09aae
Diego Guerra <diego.guerra.suarez@gmail.com>
2015-02-22 19:01:18 +0100

    create & list songs

--- lib/app.rb
+++ lib/app.rb
@@ -6,6 +6,21 @@ get "/queue" do
   erb :queue
 end
 
+get "/songs" do
+  @songs = Song.all
+  erb :"songs/index"
+end
+
+get "/song/new" do
+  @song = Song.new
+  erb :"songs/new"
+end
+
+post "/song" do
+  Song.create(params)
+  redirect_to "/songs"
+end
+
 def build_sample_queue
   one_song = Song.new("19 días y 500 noches", "Joaquín Sabina")
   other_song = Song.new("Semilla en la tierra", "Carlos Chaouen")  
