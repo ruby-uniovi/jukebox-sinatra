0d0aa2f52489c9da4e848d6031d34001b42bd9e5
Diego Guerra <diego.guerra.suarez@gmail.com>
2015-03-02 22:13:43 +0100

    add specs for new song form

--- spec/integration/song_spec.rb
+++ spec/integration/song_spec.rb
@@ -11,5 +11,41 @@ describe Song do
       expect(page).to have_content("Salt Peanuts")
     end
 
+    it "shows a link to create a new song" do
+      visit "/songs"
+      expect(page).to have_selector("a.new-song[href='/songs/new']")
+    end
+
+    it "shows a link to the jukebox" do
+      visit "/songs"
+      expect(page).to have_selector("a.to-jukebox[href='/queue']")
+    end
+
+  end
+
+  context "/songs/new" do
+
+    it "has a form with inputs for artist, title and url" do
+      visit "/songs/new"
+      expect(page).to have_selector("form input[name=artist]")
+      expect(page).to have_selector("form input[name=title]")
+      expect(page).to have_selector("form input[name=url]")
+    end
+
+    it "has a button to submit the form" do
+      visit "/songs/new"
+      expect(page).to have_selector("form button[type=submit]")
+    end
+
+    it "creates a new song when submitting the form" do
+      visit "/songs/new"
+      within "form" do
+        fill_in "artist", :with => "Dizzie Gillespie"
+        fill_in "title", :with => "Salt Peanuts"
+        fill_in "url", :with => "http://nononoexisto.com"
+        click_button("Let's Swing!")
+      end
+      expect(Song.first.display_name).to eql "Dizzie Gillespie - Salt Peanuts"
+    end
   end
 end
