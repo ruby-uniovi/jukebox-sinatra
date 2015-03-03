e4af967c7bf485c420440176754653712aec103a
Daniel Carral <dan@dcarral.org>
2015-03-03 09:50:37 +0100

    Code formatting, for the sake of consistency

--- spec/integration/song_spec.rb
+++ spec/integration/song_spec.rb
@@ -6,18 +6,22 @@ describe Song do
     it "shows a list of songs" do
       Song.create(:artist => "Art Blakey & The Jazz Messengers", :title => "A Night in Tunisia", :url => "http://example.com/nothing_here")
       Song.create(:artist => "Dizzie Gillespie", :title => "Salt Peanuts", :url => "http://example.com/nothing_here")
+
       visit "/songs"
+
       expect(page).to have_content("A Night in Tunisia")
       expect(page).to have_content("Salt Peanuts")
     end
 
     it "shows a link to create a new song" do
       visit "/songs"
+
       expect(page).to have_selector("a.new-song[href='/songs/new']")
     end
 
     it "shows a link to the jukebox" do
       visit "/songs"
+
       expect(page).to have_selector("a.to-jukebox[href='/queue']")
     end
 
@@ -27,6 +31,7 @@ describe Song do
 
     it "has a form with inputs for artist, title and url" do
       visit "/songs/new"
+
       expect(page).to have_selector("form input[name=artist]")
       expect(page).to have_selector("form input[name=title]")
       expect(page).to have_selector("form input[name=url]")
@@ -34,6 +39,7 @@ describe Song do
 
     it "has a button to submit the form" do
       visit "/songs/new"
+
       expect(page).to have_selector("form button[type=submit]")
     end
 
@@ -45,6 +51,7 @@ describe Song do
         fill_in "url", :with => "http://nononoexisto.com"
         click_button("Let's Swing!")
       end
+
       expect(Song.first.display_name).to eql "Dizzie Gillespie - Salt Peanuts"
     end
   end
--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -2,12 +2,13 @@ require 'spec_helper'
 
 describe Song do
 
-  let(:song) {Song.create(:artist => "Art Blakey & The Jazz Messengers", :title => "A Night in Tunisia", :url => "http://example.com/nothing_here")}
+  let(:song) { Song.create(:artist => "Art Blakey & The Jazz Messengers", :title => "A Night in Tunisia", :url => "http://example.com/nothing_here") }
 
   context "test running" do
     it "resets the test database each time it runs" do
       Song.create(:artist => "Velvet Underground", :title => "Venus in Furs")
       Song.create(:artist => "David Bowie", :title => "Rebel, Rebel")
+
       expect(Song.count).to eql(2)
     end
   end
@@ -29,12 +30,14 @@ describe Song do
   describe ".create_sample_songs" do
     it "creates some songs if there are none" do
       Song.create_sample_songs
+
       expect(Song.all).to_not be_empty
     end
 
     it "doesn't create songs if there are some already" do
       Song.create(:artist => "A", :title => "T", :url => "URL")
-      expect {Song.create_sample_songs}.to_not change {Song.count}
+
+      expect { Song.create_sample_songs }.to_not change { Song.count }
     end
   end
 
