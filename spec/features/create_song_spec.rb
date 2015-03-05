596dcbd4932699ed6a3a6e6ea8cfbeb68e3eeb42
Diego Guerra <diego.guerra.suarez@gmail.com>
2015-03-05 05:25:12 +0100

    split song specs in 3: index, new and create

---
+++ spec/features/create_song_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe "POST /song" do
+  it "creates a new song when submitting the form" do
+    visit "/songs/new"
+    within "form" do
+      fill_in "artist", :with => "Dizzie Gillespie"
+      fill_in "title", :with => "Salt Peanuts"
+      fill_in "url", :with => "http://nononoexisto.com"
+      click_button("Let's Swing!")
+    end
+
+    expect(Song.first.display_name).to eql "Dizzie Gillespie - Salt Peanuts"
+  end
+
+end
